0d462f5a3e59c8c45161d8a2d01aa3e3217de08d
thomas dubiel <td1729@hotmail.com>
2020-01-14 12:03:19 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram 
+  attr_accessor :word 
+  
+  def initialize(word) 
+    @word = word 
+  end
+  
+  def match(possible_matches) 
+    anagrams = []
+    possible_matches.each do |word| 
+      split_word = word.split.sort 
+      puts split_word
+      if @word.split.sort == split_word 
+        anagrams << split_word 
+      end 
+    end
+    anagrams 
+  end
+end
