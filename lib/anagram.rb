ae19d695f49742302d725994d7cbc789a5c9ab54
thomas dubiel <td1729@hotmail.com>
2020-01-14 12:14:42 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,8 +11,9 @@ class Anagram
     possible_matches.each do |word| 
       split_word = word.split("") 
       sorted_split_word = split_word.sort
-      puts "the sorted test word is #{sorted_split_word 
-      puts @word 
+      puts "the sorted test word is #{sorted_split_word}" 
+      
+      puts "the sorted main word is #{@word}" 
       if @word.split.sort == sorted_split_word 
         anagrams << split_word 
       end 
